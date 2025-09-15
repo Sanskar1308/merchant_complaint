81acb12b474ea7ba2970d08a33b11f50962f7efb
Sanskar Chirania <99312244+Sanskar1308@users.noreply.github.com>
2025-09-15 13:11:58 +0530

    Fix AxiosResponse type import error

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { type AxiosResponse } from 'axios';
 import { useAuthStore } from '../stores/authStore';
 import type { 
   ApiResponse, 
